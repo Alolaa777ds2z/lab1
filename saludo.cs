f25311b20ef7ec2d2af4b3f4cf1e74b6226f2f29
Alolaa777ds2z <alola343xdxs@gmail.com>
2026-09-01 10:06:40 -0500

    Add files via upload

---
+++ saludo.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Esciba un numero1: ");
+int numero1 = int.Parse(Console.ReadLine());
+
+if (numero1 <0)
+{
+    Console.WriteLine("no eres bienvenido");
+
+} 
+
+else
+{
+    while (numero1 > 0)
+    {
+        Console.WriteLine("bienvenido");
+        break;
+    }
+    
+}
